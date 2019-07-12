0d75da051fd96a4d20b40a758657641ae45236e9
Muhammad Iqbal Jamil -X (muhjamil - CAPGEMINI TECHNOLOGY SERVICES INDIA LIMITED at Cisco) <muhjamil@cisco.com>
2019-07-12 11:47:18 +0530

    Initial skeleton for CP

---
+++ src/main/java/com/cisco/cxcp/CxcpApplication.java
@@ -0,0 +1,13 @@
+package com.cisco.cxcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CxcpApplication.class, args);
+	}
+
+}
